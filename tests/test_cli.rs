52d33ff328007c0ad203bebdf82c18809650161e
Abid Omar <contact@omarabid.com>
2025-06-03 14:05:59 +0800

    refactor: remove cli tests

--- tests/test_cli.rs
+++ tests/test_cli.rs
@@ -6,16 +6,3 @@ use assert_cmd::prelude::*;
 use predicates::prelude::*;
 
 use std::process::Command;
-
-#[test]
-fn test_cli() {
-    let mut cmd = Command::cargo_bin("codeinput").expect("Calling binary failed");
-    cmd.assert().failure();
-}
-
-#[test]
-fn test_version() {
-    let expected_version = "codeinput 0.0.1-beta\n";
-    let mut cmd = Command::cargo_bin("codeinput").expect("Calling binary failed");
-    cmd.arg("--version").assert().stdout(expected_version);
-}
